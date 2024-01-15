c24476332b7d69048cc4cae1a8bad4ceb4d2f76c
Franco Aguirre <fn.aguirre@hotmail.com>
2024-01-15 19:04:20 -0300

    fuentes y colores configurados

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,13 @@ export default {
     ],
     theme: {
       extend: {},
+      colors:{
+        'black': '#000000',
+        'semi': '#0C1821',
+        'blue':'#1B2A41',
+        'blueLight':'#324A5F',
+        'gray':'#CCC9DC'
+      }
     },
     plugins: [],
   }
